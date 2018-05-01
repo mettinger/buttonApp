ff51636a7cd33b86b4eb77c45a5316b8c7d212a7
mettinger <jettinger35@gmail.com>
2018-05-01 13:27:40 -0400

    message

--- app/src/main/java/com/example/marke/buttontest/MainActivity.java
+++ app/src/main/java/com/example/marke/buttontest/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.marke.buttontest;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -16,8 +15,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList timeArray = new ArrayList();
-    //String myURL = "http://52.87.117.189:8890/?data="; // spoken aws machine
-    String myURL = "http://18.208.37.82:8890/?data="; //personal aws machine
+    String myURL = "http://www.twobuttons.org:8890/?data="; //personal aws machine
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +24,21 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
+    // HANDLE BUTTON 0 PRESS
     public void doButton0(View view){
 
-        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date()) + "-0";
-        EditText editText = findViewById(R.id.editText);
-        timeArray.add(timeStamp);
-        editText.setText(timeStamp);
+        final String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date()) + "-0";
+        EditText timeStampBox = findViewById(R.id.editText);
+        timeStampBox.setText(timeStamp);
+
+        EditText infoBox = findViewById(R.id.editText2);
+        final String info = String.valueOf(infoBox.getText());
 
         new Thread(new Runnable(){
             @Override
             public void run() {
                 try {
-                    String dataString = TextUtils.join(", ", timeArray);
+                    String dataString = timeStamp + "-" + info;
                     URL url = new URL(myURL + dataString);
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     con.setRequestMethod("GET");
@@ -52,18 +53,21 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+    // HANDLE BUTTON 1 PRESS
     public void doButton1(View view){
 
-        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date()) + "-1";
-        EditText editText = findViewById(R.id.editText);
-        timeArray.add(timeStamp);
-        editText.setText(timeStamp);
+        final String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date()) + "-1";
+        EditText timeStampBox = findViewById(R.id.editText);
+        timeStampBox.setText(timeStamp);
+
+        EditText infoBox = findViewById(R.id.editText2);
+        final String info = String.valueOf(infoBox.getText());
 
         new Thread(new Runnable(){
             @Override
             public void run() {
                 try {
-                    String dataString = TextUtils.join(", ", timeArray);
+                    String dataString = timeStamp + "-" + info;
                     URL url = new URL(myURL + dataString);
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     con.setRequestMethod("GET");
@@ -77,27 +81,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
     }
-
-    /*
-    public void recordData(View view){
-
-        new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try {
-                    String dataString = TextUtils.join(", ", timeArray);
-                    URL url = new URL(myURL + dataString);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod("GET");
-                    int status = con.getResponseCode();
-                    con.disconnect();
-                    timeArray.clear();
-                }
-                catch (Exception ex) {
-                    int placeholder = 1;
-                }
-            }
-        }).start();
-    } */
 
 }
